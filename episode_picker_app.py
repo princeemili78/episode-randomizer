04050b27d906544328137c7ccc97a4b8c11434f8
Prince Emili <princeemili78@gmail.com>
2026-03-27 19:26:00 -0400

    add streamlit app

--- episode_picker_app.py
+++ episode_picker_app.py
@@ -1,20 +1,11 @@
 import streamlit as st
 from episode_puller_v2 import TvShow
 
-
+# textbox for user to input show name
 show_name = st.text_input("Name of Tv Show")
 
+# create instance of tv show using user input
 show = TvShow(show_name)
 
-page_bg_img = '''
-<style>
-body {
-background-image: url(show.picture);
-background-size: cover;
-}
-</style>
-'''
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.write(str(show.title_id))
